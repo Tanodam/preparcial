a88a3500ab04332642fb68c7a7d465861911e766
tanodam <desariod95@gmail.com>
2020-04-30 20:43:08 -0300

    segundo commit

--- src/app/services/servicio.service.ts
+++ src/app/services/servicio.service.ts
@@ -7,12 +7,13 @@ import {HttpClient} from '@angular/common/http';
 })
 export class ServicioService {
 
-  public peliculas:Peliculas[] = [{"id": 1, "nombre": "La guerra de las galaxias","tipo": 4,"fechaDeEstreno":"27/02/1990","cantidadDePublico":1450000,"fotoDeLaPelicula":"c/sarasa/carpeta"}]
+  public peliculas:Peliculas[] = [{"id": 1, "nombre": "La guerra de las galaxias","tipo": 4,"fechaDeEstreno":"27/02/1990","cantidadDePublico":1450000,"fotoDeLaPelicula":"c/sarasa/carpeta"},
+  {"id": 2, "nombre": "La guerra de las galaxias","tipo": 4,"fechaDeEstreno":"27/02/1990","cantidadDePublico":1450000,"fotoDeLaPelicula":"c/sarasa/carpeta"}]
 
   constructor(private http:HttpClient) { }
 
   public traerPeliculas()
   {
-    
+    return this.peliculas;
   }
 }
